c132cd0f694db0af0420608d5c292ebb327bd77b
defaultbahaz <mbahadirdursun2@gmail.com>
2026-07-19 17:26:37 +0300

    fix: useReducer with code config

--- src/App.tsx
+++ src/App.tsx
@@ -12,32 +12,14 @@ function App() {
 
   return (
     <Background>
-      <style>
-        {`
-          html, body, *, button, a, input,
-          .disabled, a:disabled, input:disabled {
-            cursor: url('/maincursor.svg'), auto;
-          }
-
-          button:hover, a:hover, input:hover, .hover:hover,
-          button:focus, a:focus, input:focus, .focus:focus  {
-            cursor: url('/hovercursor.svg'), auto;
-          }
-
-          input[type="text"], input[type="email"], input[type="password"],
-          input[type="search"], textarea, [contenteditable] {
-            cursor: url('/textcursor.svg') 12 12, text;
-          }
-        `}
-      </style>
       <Content>
         <Card>{texts.content.texts.title}</Card>
         <Card>
-          <CodeHeader></CodeHeader>
+          <CodeHeader/>
           <Code></Code>
         </Card>
       </Content>
-      <Footer></Footer>
+      <Footer/>
     </Background>
   )
 }
--- src/components/Code/Code.tsx
+++ src/components/Code/Code.tsx
@@ -1,18 +1,47 @@
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import style from './Code.module.css';
+import { useMemo, useReducer } from 'react';
+type Config = {
+  noUnusedLocals: boolean;
+  noUnusedParameters: boolean;
+  composite: boolean;
+};
 
+type Action =
+  | { type: "toggle"; key: keyof Config }
+  | { type: "reset" };
+
+const initialState: Config = {
+  noUnusedLocals: true,
+  noUnusedParameters: true,
+  composite: true,
+};
+
+function reducer(state: Config, action: Action): Config {
+  switch (action.type) {
+    case "toggle":
+      return {
+        ...state,
+        [action.key]: !state[action.key],
+      };
+
+    case "reset":
+      return initialState;
+
+    default:
+      return state;
+  }
+}
 export function Code() {
+    const [config, dispatch] = useReducer(reducer, initialState)
+      const code = useMemo(
+        () => JSON.stringify(config, null, 4)
+        ,[config]);
     return(
-        <div>
-            <SyntaxHighlighter language='json' style={docco} showLineNumbers>
-                
-                    "noUnusedLocals": true,
-                    "noUnusedParameters": true,
-                    "erasableSyntaxOnly": true,
-                    "noFallthroughCasesInSwitch": true,
-                    "composite": true,
-                    "ignoreDeprecations": "6.0"
-                
+        <div className={style.code}>
+            <SyntaxHighlighter id="code" language='json' style={docco} showLineNumbers>
+                {code}
             </SyntaxHighlighter>
         </div>
     )
